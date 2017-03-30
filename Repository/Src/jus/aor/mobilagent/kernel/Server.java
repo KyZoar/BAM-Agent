5b7f0403bd18290e717044c45d9b6b600094820c
Stractus <charles-marchand@hotmail.fr>
2017-03-30 14:47:12 +0200

    DIT A LUCAS QU'IL FERME SA GUEULE STP

--- Repository/Src/jus/aor/mobilagent/kernel/Server.java
+++ Repository/Src/jus/aor/mobilagent/kernel/Server.java
@@ -93,7 +93,7 @@ public final class Server implements _Server{
 	 * @throws Exception
 	 */
 	protected void startAgent(_Agent agent, BAMAgentClassLoader loader) throws Exception {
-		//A COMPLETER
+		agent.move(loader.getResource(this.name));
 	}
 	@Override
 	public void deployAgent(String className, Object[] args,
