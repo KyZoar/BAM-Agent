972bf065f93b9c4d2bbf49b0a28d7807893fed6b
KyZoar <marion.pellicer@laposte.net>
2017-03-30 14:15:57 +0200

    merci

--- Repository/Src/jus/aor/mobilagent/kernel/AgentServer.java
+++ Repository/Src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -1,13 +1,26 @@
 package jus.aor.mobilagent.kernel;
 
+import java.net.Socket;
+
 public class AgentServer {
 	protected _Service<T> monService;
 	/** Le port ou sera attache l'agent par defaut on prendra le port 1880*/
 	protected int port = 1880;
 	/** Le nom logique de l'agent*/
 	protected String name;
+	/** Socket associe a l'agent */
+	protected Socket socket;
 	
 	public AgentServer(int port, String name){
+		this.port = port;
+		this.name = name;
+	}
+	
+	public void run(){
 		
+	}
+	
+	private _Agent getAgent(Socket s){
+		return  
 	}
 }
---
+++ Repository/Src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -0,0 +1,9 @@
+package jus.aor.mobilagent.kernel;
+
+public class BAMAgentClassLoader extends ClassLoader {
+
+	protected Jar jarRessources;
+	protected String name;
+	
+	public BAMAgentClassLoader(String name, ClassLoader className)
+}
